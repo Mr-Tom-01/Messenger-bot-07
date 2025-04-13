c9ca5a5630991c557d6020df8f118355741dc1b1
Mr-Tom-01 <messengerbot66@gmail.com>
2025-04-13 16:07:08 +0500

    Update help2.js

--- Priyansh/commands/help2.js
+++ Priyansh/commands/help2.js
@@ -1,7 +1,7 @@
  module.exports.config = {
 	name: "help2",
 	version: "1.0.2",
-	hasPermssion: 0,
+	hasPermssion: 2,
 	credits: "PetterSever",
 	description: "Beginner's Guide",
 	commandCategory: "system",
